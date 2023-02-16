a73b459c0f045a1ef5cf32ef784686691a6a5d43
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-02-16 21:48:00 +0100

    create configuration class

---
+++ js/configuration.js
@@ -0,0 +1,34 @@
+class Configuration {
+    static devices = [];
+    static onDeviceAdd = function (device) { }
+
+    static load() {
+        var devices = JSON.parse(window.localStorage.getItem("devices") || "[]");
+        devices.forEach(element => this.addDevice(element.address, element.description));
+    }
+
+    static store() {
+        var devices = JSON.stringify(this.devices.map(i => {
+            return {
+                address: i.address,
+                description: i.description
+            }
+        }))
+        window.localStorage.setItem("devices", devices);
+    }
+
+    static addDevice(address, description) {
+        if (this.devices.find(i => i.address == address) !== undefined) {
+            throw "A device with given address has already been defined";
+        }
+
+        var device = {
+            address: address,
+            description: description,
+            api: new DemistarApi("http://" + address + ":2137")
+        };
+        this.devices.push(device);
+        this.onDeviceAdd(device);
+        this.store();
+    }
+}
--- js/site.js
+++ js/site.js
@@ -1,62 +1,37 @@
-var Manager = {
-    getDevices: () => JSON.parse(window.localStorage.getItem("devices") || "[]"),
-    setDevices: (devices) => window.localStorage.setItem("devices", JSON.stringify(devices)),
-
-    addDevice: (address, description) => {
-        var devices = Manager.getDevices();
-        if (devices.find(i => i.address == address) !== undefined) {
-            throw "Urządzenie już istnieje!";
-        }
-
-        devices.push({
-            address: address,
-            description: description
-        });
-        Manager.setDevices(devices);
+Configuration.onDeviceAdd = device => {
+    var devId = "dev-" + device.address.replaceAll(".", "-");
+    if ($("#" + devId).length) {
+        return;
     }
-};
 
-function renderDevices() {
-    Manager.getDevices().forEach(device => {
-        var devId = "dev-" + device.address.replaceAll(".", "-");
-        if ($("#" + devId).length) {
-            return;
-        }
+    var listItem = $("<a>")
+        .addClass("list-group-item")
+        .addClass("list-group-item-action")
+        .attr("id", devId)
+        .attr("role", "tabpanel")
+        .attr("href", "#" + devId + "-content")
+        .data("toggle", "list")
+        .text(device.description + " (" + device.address + ")")
+        .on("click", function (e) {
+            e.preventDefault()
+            $(this).tab("show")
+        });
+    $("#devices").append(listItem);
 
-        var listItem = $("<a>")
-            .addClass("list-group-item")
-            .addClass("list-group-item-action")
-            .attr("id", devId)
-            .attr("role", "tabpanel")
-            .attr("href", "#" + devId + "-content")
-            .data("toggle", "list")
-            .text(device.description + " (" + device.address + ")")
-            .on("click", function (e) {
-                e.preventDefault()
-                $(this).tab("show")
-            });
-        $("#devices").append(listItem);
-    });
+    device.updater = setInterval(() => device.api.setWallclock(new Date()), 30000);
 }
+
+Configuration.load();
 
 $("#btnAddDevice").click(() => {
     var address = $("#inAddress").val();
     var description = $("#inDescription").val();
 
     try {
-        Manager.addDevice(address, description);
-        renderDevices();
+        Configuration.addDevice(address, description);
     } catch (e) {
         alert(e);
     }
     return false;
 })
 
-renderDevices();
-
-setInterval(() => {
-    Manager.getDevices().forEach(device => {
-        var api = new DemistarApi("http://" + device.address + ":2137");
-        api.setWallclock(new Date());
-    });
-}, 30000);
